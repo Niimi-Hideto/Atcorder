ba32db93282bad960f23716522ef2d8e092d7f88
Hideto_Niimi <0418Hideto@gmail.com>
2026-06-07 00:02:44 +0900

    ABC460, 461を追加

---
+++ ABC_A/ABC461.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, D;
+    cin >> A >> D;
+
+    if (A > D)
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+}
