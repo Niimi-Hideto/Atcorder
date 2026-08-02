a209fce43e2093d394ece40323834fef3a261437
Hideto_Niimi <0418Hideto@gmail.com>
2026-08-03 01:26:45 +0900

    B問題の復讐

--- ABC_Re_B/B450.cpp
+++ ABC_Re_B/B450.cpp
@@ -4,48 +4,29 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    vector<vector<int>> vec(n + 1, vector<int>(n + 1));
 
-    vector<vector<int>> c(n - 1, vector<int>(n));
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i; j < n - 1; j++) {
-            cin >> c.at(i).at(j);
+    for (int i = 1; i < n; i++) {
+        for (int j = i + 1; j < n + 1; j++) {
+            cin >> vec.at(i).at(j);
         }
     }
 
-    for (int i = 0; i < n - 2; i++) {
-        for (int j = i; j < n - 2; j++) {
-            for (int k = i + 1; j < n - 1; j++) {
-            }
-        }
-    }
-}
-
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-
-    vector<vector<long long>> cost(n, vector<long long>(n, 0));
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            cin >> cost.at(i).at(j);
-        }
-    }
-
-    bool found = false;
-    for (int a = 0; a < n - 2; a++) {
-        for (int b = a + 1; b < n - 1; b++) {
-            for (int c = b + 1; c < n; c++) {
-                if (cost.at(a).at(b) + cost.at(b).at(c) < cost.at(a).at(c)) {
-                    found = true;
+    bool flag = false;
+    for (int a = 1; a <= n - 2; a++) {
+        for (int b = a + 1; b <= n - 1; b++) {
+            for (int c = b + 1; c <= n; c++) {
+                if (vec.at(a).at(c) > vec.at(a).at(b) + vec.at(b).at(c)) {
+                    flag = true;
                 }
             }
         }
     }
 
-    cout << (found ? "Yes" : "No") << endl;
+    if (flag) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
 }
-*/
--- ABC_Re_B/B460.cpp
+++ ABC_Re_B/B460.cpp
@@ -6,49 +6,17 @@ int main() {
     cin >> t;
 
     for (int i = 0; i < t; i++) {
-        long long x1, x2, y1, y2, r1, r2;
-        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-
-        long long R = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
-
-        if ((x1 + r1 <= x2) || ((y1 + r1 <= y2))) {
-            long long R = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
-            if (R <= ((r1 * r1) + (2 * r1 * r2) + (r2 * r2))) {
-                cout << "Yes" << endl;
-            }
-            else {
-                cout << "No" << endl;
-            }
-        }
-        else {
-            if (R >= ((r1 * r1) - (2 * r1 * r2) + (r2 * r2))) {
-                cout << "Yes" << endl;
-            }
-            else {
-                cout << "No" << endl;
-            }
-        }
-    }
-}
-
-/*
-int main() {
-    int t;
-    cin >> t;
-
-    for (int i = 0; i < t; i++) {
         long long x1, y1, r1, x2, y2, r2;
         cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
-        long long d2 = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
-        long long lo = (r1 - r2) * (r1 - r2);
-        long long hi = (r1 + r2) * (r1 + r2);
+        long long d = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
 
-        if (lo <= d2 && d2 <= hi) {
+        if ((r2 * r2) - (2 * r2 * r1) + (r1 * r1) <= d
+            && d <= (r2 * r2) + (2 * r2 * r1) + (r1 * r1)) {
             cout << "Yes" << endl;
-        } else {
+        }
+        else {
             cout << "No" << endl;
         }
     }
 }
-*/
